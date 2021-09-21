5f7cf999828daa1ddfa9ec44aa39d17bc8befa5f
p22jiacono <p22jiacono@fairfieldprep.org>
2021-09-21 10:05:28 -0400

    lab6-1

---
+++ lab_6-1.py
@@ -0,0 +1,10 @@
+# Author: JTI 9/21/21
+
+firstname = input ("What is your first name? ")
+print(firstname)
+
+lastname = input ("What is your last name? ")
+print(lastname)
+
+fullname = firstname + " " + lastname
+print (fullname)
